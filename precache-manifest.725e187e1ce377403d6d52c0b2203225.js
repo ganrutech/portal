bd12b638004310bb73f17e855793ede6a476f5f0
Guruprakash <guruprakash.narendran@ticvic.com>
2020-10-03 12:56:57 +0530

    Updates

---
+++ precache-manifest.725e187e1ce377403d6d52c0b2203225.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7180631eeb935c5ac9a606612be0a3b",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "b8922f25cad8d8031769",
+    "url": "/portal/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6f1f54a01ebeb85f16a0",
+    "url": "/portal/static/css/main.b0a88121.chunk.css"
+  },
+  {
+    "revision": "b8922f25cad8d8031769",
+    "url": "/portal/static/js/2.052a3c7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portal/static/js/2.052a3c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1f54a01ebeb85f16a0",
+    "url": "/portal/static/js/main.97edce36.chunk.js"
+  },
+  {
+    "revision": "78d9e659bdc1faae70c2",
+    "url": "/portal/static/js/runtime-main.254bcb09.js"
+  },
+  {
+    "revision": "1943b47ab74ab7e2dd62f21576fae136",
+    "url": "/portal/static/media/logo.1943b47a.png"
+  }
+]);
